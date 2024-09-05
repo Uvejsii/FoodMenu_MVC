a29c4107ef33a3f13caf3fdfbae29e2aacf1c850
Uvejsii <136826106+Uvejsii@users.noreply.github.com>
2024-09-05 18:08:52 +0200

    Add project files.

---
+++ FoodMenu/Controllers/FoodMenu.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FoodMenu.Data;
+using FoodMenu.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodMenu.Controllers
+{
+    public class FoodMenu : Controller
+    {
+        private readonly FoodMenuContext _context;
+        public FoodMenu(FoodMenuContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dishes.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var dish = await _context.Dishes
+                .Include(di => di.DishIngredients)
+                .ThenInclude(i => i.Ingredient)
+                .FirstOrDefaultAsync(d => d.Id == id);
+
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+    }
+}
---
+++ FoodMenu/Data/FoodMenuContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FoodMenu.Models;
+
+namespace FoodMenu.Data
+{
+    public class FoodMenuContext : DbContext
+    {
+        public FoodMenuContext(DbContextOptions<FoodMenuContext> options) : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishIngredient>().HasKey(di => new 
+            {
+                di.DishId,
+                di.IngredientId
+            });
+
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+            modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.DishIngredients).HasForeignKey(i => i.IngredientId);
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish { Id = 1, Name = "Margheritta", Price = 7.50, ImageUrl = "https://img.taste.com.au/PwXf3RRU/w720-h480-cfill-q80/taste/2016/11/eat-pray-love-39581-3.jpeg" }
+                );
+            modelBuilder.Entity<Ingredient>().HasData(
+                new Ingredient { Id = 1, Name = "Tomato Sauce"},
+                new Ingredient { Id = 2, Name = "Mozzarella" }
+                );
+            modelBuilder.Entity<DishIngredient>().HasData(
+                new DishIngredient { DishId = 1, IngredientId = 1 },
+                new DishIngredient { DishId = 1, IngredientId = 2 }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<DishIngredient> DishIngredients { get; set; }
+    }
+}
